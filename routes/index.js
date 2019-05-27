351e72698e3d285cefbef0ea1d2dd7970e7737cb
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-27 15:59:42 +0700

    dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
+const Story = require('../models/Story');
+
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome');
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('index/dashboard');
+  Story.find({ user: req.user.id })
+    .then(stories => {
+      res.render('index/dashboard', { stories });
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/about', (req, res) => {
